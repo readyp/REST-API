6a72193090c2ad9d64ce813e87f9ab1a325ef140
readyp <readypamungkas@gmail.com>
2023-02-07 13:30:42 +0700

    Add connection to mongoDB, add middleware body parse, and unhadledPromiseRejection

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
 
 const bootcampsRouter = require("./router/bootcamps");
 const errorHandler = require("./middleware/errorHandler");
+const connectDB = require("./config/db");
 
 // load vars
 dotenv.config({ path: "./config/config.env" });
@@ -14,16 +16,31 @@ const app = express();
 // logger middleware using morgan
 app.use(morgan("dev"));
 
+// body parser
+app.use(express.json());
+
 // bootcamps router
 app.use("/api/v1/bootcamps", bootcampsRouter);
 
 // error handler middleware
 app.use(errorHandler);
 
+// connectDB
+connectDB();
+
 // start up server
-app.listen(
+const server = app.listen(
   process.env.PORT || 5000,
   console.log(
-    `Server is up in ${process.env.NODE_ENV} on PORT: ${process.env.PORT}`
+    `Server is up in ${process.env.NODE_ENV.bold} on PORT: ${process.env.PORT}`
+      .yellow
   )
 );
+
+// Unhandled Promise Rejected
+process.on("unhandledRejection", () => {
+  server.close(() => {
+    console.log("Unhandle Promise rejection");
+    process.exit(1);
+  });
+});
