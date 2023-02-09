ab645331f7c5d87895aa507cecec95f9fcfe0718
readyp <readypamungkas@gmail.com>
2023-02-09 19:12:45 +0700

    Add get bootcamp by radius

--- router/bootcamps.js
+++ router/bootcamps.js
@@ -6,18 +6,22 @@ const {
   createBootcamps,
   updateBootcamps,
   deleteBootcamps,
+  getAllBootcampsByRadius,
 } = require("../controller/bootcamps");
 const coursesRouter = require("./courses");
+
 const router = express.Router();
 
 // router use course router
 router.use("/:bootcampId/courses", coursesRouter);
 
+router.route("/radius/:zipcode/:distance").get(getAllBootcampsByRadius);
+
 router.route("/").get(getAllBootcamps).post(createBootcamps);
+
 router
   .route("/:id")
   .get(getOneBootcamps)
   .put(updateBootcamps)
   .delete(deleteBootcamps);
-
 module.exports = router;
